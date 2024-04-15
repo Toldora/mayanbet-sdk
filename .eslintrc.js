b599e42bc622c9a093a7b2711f00b09af1a0bea7
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2024-04-15 15:40:18 +0300

    added base files

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:import/recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  settings: {
+    'import/resolver': {
+      jsconfig: {
+        config: './jsconfig.json',
+      },
+    },
+  },
+  rules: {
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'arrow-parens': 0,
+    'no-debugger': 1,
+    'no-warning-comments': [
+      1,
+      {
+        terms: ['hardcoded'],
+        location: 'anywhere',
+      },
+    ],
+    'no-return-await': 0,
+    'object-curly-spacing': ['error', 'always'],
+    'no-var': 'error',
+    'comma-dangle': [1, 'always-multiline'],
+    'linebreak-style': ['error', 'unix'],
+    'max-len': [
+      1,
+      {
+        code: 80,
+        comments: 80,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+    'no-console': [
+      1,
+      {
+        allow: ['warn', 'error'],
+      },
+    ],
+  },
+};
