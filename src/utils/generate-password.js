7a28da1cb0c1d3aef26ac6a5c28f17d44096a15c
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2024-04-30 20:06:59 +0300

    improved password func

--- src/utils/generate-password.js
+++ src/utils/generate-password.js
@@ -1,10 +1,10 @@
 const CHARS = '0123456789';
 const PASSWORD_LENGTH = 6;
 
-export const generatePassword = () => {
+export const generatePassword = (passwordLength = PASSWORD_LENGTH) => {
   let password = '';
 
-  for (let i = 0; i < PASSWORD_LENGTH; i += 1) {
+  for (let i = 0; i < passwordLength; i += 1) {
     const randomNumber = Math.floor(Math.random() * CHARS.length);
     password += CHARS.substring(randomNumber, randomNumber + 1);
   }
